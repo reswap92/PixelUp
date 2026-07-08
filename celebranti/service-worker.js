26fb570b5131462c8ba7f81e4454a5c707d3e93c
reswap92 <vincenzorusso.0510@gmail.com>
2026-07-08 12:53:13 +0200

    Create service-worker.js

---
+++ celebranti/service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "pixelup-celebranti-v1";
+const ASSETS = [
+  "/celebranti/",
+  "/celebranti/index.html",
+  "/celebranti/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => null)
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
